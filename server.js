a9950b578e3e81652541bb1a4864475764e2a563
Ann Duong <>
2018-05-31 10:46:41 -0400

    set up, start script, heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -33,6 +34,8 @@ hbs.registerHelper('getCurrentYear', () => {
 });
 
 
+
+
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 })
@@ -58,4 +61,6 @@ app.get('/bad', (request, response) => {
   });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
